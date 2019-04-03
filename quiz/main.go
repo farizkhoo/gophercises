6b7a7c536eb287aa168bdc30c11be40bd59d5ea1
farizkhoo <fariz_khoo@hotmail.com>
2019-04-04 00:24:52 +0800

    initial commit

---
+++ quiz/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Let's start the quiz!")
+	csvFile, err := os.Open("problems.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := csv.NewReader(csvFile)
+	ch := make(chan string)
+	reader := bufio.NewReader(os.Stdin)
+	prob, err := r.Read()
+
+	if err != nil {
+		return
+	}
+	fmt.Printf("Problem #1: %v=?\n", prob[0])
+	s, _ := reader.ReadString('\n')
+	s = strings.TrimSuffix(s, "\r\n")
+	go checker(s, prob[1], ch)
+	fmt.Println(<-ch)
+}
+
+func checker(s, ans string, ch chan string) {
+	if s == ans {
+		ch <- "CORRECT!"
+	} else {
+		ch <- "WRONG!"
+	}
+}
